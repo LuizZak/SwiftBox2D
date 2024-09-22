4101f7cf4e7988a8c38213bf42d76f0de40ed990
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2024-09-22 08:13:03 -0300

    Adding arithmetic operators to b2Vec2

--- Sources/SwiftBox2D/Extensions/b2Vec2+Ext.swift
+++ Sources/SwiftBox2D/Extensions/b2Vec2+Ext.swift
@@ -5,3 +5,84 @@ extension b2Vec2 {
         b2Vec2_zero
     }
 }
+
+extension b2Vec2 {
+    @inlinable
+    public static func + (lhs: Self, rhs: Self) -> Self {
+        binaryOp(lhs, rhs, +)
+    }
+
+    @inlinable
+    public static func - (lhs: Self, rhs: Self) -> Self {
+        binaryOp(lhs, rhs, -)
+    }
+
+    @inlinable
+    public static func * (lhs: Self, rhs: Self) -> Self {
+        binaryOp(lhs, rhs, *)
+    }
+
+    @inlinable
+    public static func / (lhs: Self, rhs: Self) -> Self {
+        binaryOp(lhs, rhs, /)
+    }
+
+    // MARK: Scalars
+
+    @inlinable
+    public static func + (lhs: Self, rhs: Float) -> Self {
+        binaryOp(lhs, rhs, +)
+    }
+
+    @inlinable
+    public static func + (lhs: Float, rhs: Self) -> Self {
+        binaryOp(lhs, rhs, +)
+    }
+
+    @inlinable
+    public static func - (lhs: Self, rhs: Float) -> Self {
+        binaryOp(lhs, rhs, -)
+    }
+
+    @inlinable
+    public static func - (lhs: Float, rhs: Self) -> Self {
+        binaryOp(lhs, rhs, -)
+    }
+
+    @inlinable
+    public static func * (lhs: Self, rhs: Float) -> Self {
+        binaryOp(lhs, rhs, *)
+    }
+
+    @inlinable
+    public static func * (lhs: Float, rhs: Self) -> Self {
+        binaryOp(lhs, rhs, *)
+    }
+
+    @inlinable
+    public static func / (lhs: Self, rhs: Float) -> Self {
+        binaryOp(lhs, rhs, /)
+    }
+
+    @inlinable
+    public static func / (lhs: Float, rhs: Self) -> Self {
+        binaryOp(lhs, rhs, /)
+    }
+
+    // MARK: Internal
+
+    @inlinable
+    static func binaryOp(_ lhs: Self, _ rhs: Self, _ op: (Float, Float) -> Float) -> Self {
+        .init(x: op(lhs.x, rhs.x), y: op(lhs.y, rhs.y))
+    }
+
+    @inlinable
+    static func binaryOp(_ lhs: Float, _ rhs: Self, _ op: (Float, Float) -> Float) -> Self {
+        .init(x: op(lhs, rhs.x), y: op(lhs, rhs.y))
+    }
+
+    @inlinable
+    static func binaryOp(_ lhs: Self, _ rhs: Float, _ op: (Float, Float) -> Float) -> Self {
+        .init(x: op(lhs.x, rhs), y: op(lhs.y, rhs))
+    }
+}
