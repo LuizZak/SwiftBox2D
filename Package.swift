af6c3d9f940406e35f4b98b5b38f80ae00aabbc3
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2026-01-23 12:33:05 -0300

    Adding minimum platform to manifest

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftBox2D",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13)],
     products: [
         .library(
             name: "box2d",
